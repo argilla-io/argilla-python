91802ef59778950c2d28a4749b9fa767158d8223
Paco Aranda <francis@argilla.io>
2024-01-16 10:34:11 +0100

    feat: http client

---
+++ src/argilla_sdk/_api/_http/_client.py
@@ -0,0 +1,41 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from dataclasses import dataclass
+
+import httpx
+
+_DEFAULT_API_KEY = "argilla.apikey"
+_DEFAULT_API_URL = "https://localhost:6900"
+
+
+@dataclass
+class HTTPClientConfig:
+    """Basic configuration for the HTTP client."""
+
+    api_url: str = None
+    api_key: str = None
+
+    def __post_init__(self):
+        self.api_url = self.api_url or _DEFAULT_API_URL
+        self.api_key = self.api_key or _DEFAULT_API_KEY
+
+
+def create_http_client(api_url: str, api_key: str, timeout: int, **client_args) -> httpx.Client:
+    """Initialize the SDK with the given API URL and API key."""
+    # This piece of code is needed to make old sdk works in combination with new one
+
+    headers = client_args.pop("headers", {})
+    headers["X-Argilla-Api-Key"] = api_key
+
+    return httpx.Client(base_url=api_url, timeout=timeout, headers=headers, **client_args)
---
+++ src/argilla_sdk/_api/_http/_helpers.py
@@ -0,0 +1,17 @@
+import httpx
+from httpx import HTTPStatusError
+
+
+__all__ = ["raise_for_status"]
+
+
+def raise_for_status(response: httpx.Response) -> None:
+    """Raise an exception if the response status code is not a 2xx code."""
+    try:
+        response.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        if e.response.is_client_error:
+            message = (f"{e.response.json()} {e!r}",)
+        else:
+            message = f"{e!r}. Response: {e.response.content!r}"
+        raise HTTPStatusError(message=message, request=e.request, response=e.response) from e

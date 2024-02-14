6ec2563059ba8e105a14845b19ab0d0dc8f26afe
Ben Burtenshaw <ben@argilla.io>
2024-02-14 16:57:52 +0100

    test: create and get in client interface

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,9 +1,7 @@
-import pytest
+import uuid
 
+from unittest import mock
 import argilla_sdk as rg
-from unittest import mock
-import pytest
-from unittest import mock
 
 
 def test_default_client():
@@ -12,22 +10,37 @@ def test_default_client():
         mock_client.return_value.api_key = "admin.apikey"
         mock_client.return_value.workspace = "argilla"
 
-        client = rg.Client(api_rul="http://localhost:6900", api_key="admin.apikey")
+        client = rg.Client(api_url="http://localhost:6900", api_key="admin.apikey")
         assert client.api_url == "http://localhost:6900"
         assert client.api_key == "admin.apikey"
         assert client.workspace == "argilla"
 
 
 def test_default_client_create_workspace():
-    with mock.patch("argilla_sdk.Client") as mock_client:
-        client = mock_client.return_value
-        workspace = mock.Mock()
-        client.workspace.create.return_value = workspace
+    mock_uuid = uuid.uuid4()
+    mock_name = "test-workspace"
+    mock_return = {"id": mock_uuid, "name": mock_name}
+    with mock.patch("argilla_sdk.client._api.Workspace.create") as patch:
+        patch.return_value = mock_return
+        client = rg.Client(api_url="http://test_url", api_key="admin.apikey")
+        workspace = rg.Workspace(name=mock_name, id=mock_uuid)
+        client.workspaces.create(workspace)
+        patch.assert_called_once_with(workspace)
 
-        client = rg.Client(api_rul="http://localhost:6900", api_key="admin.apikey")
-        client.workspace.create(workspace)
-        assert client.workspace.get(name="MyWorkspace") == workspace
-        assert client.workspace.get(name="MyWorkspace") == workspace
+
+def test_default_client_create_workspace_then_get():
+    mock_uuid = uuid.uuid4()
+    mock_name = "test-workspace"
+    mock_return = {"id": mock_uuid, "name": mock_name}
+    with mock.patch("argilla_sdk.client._api.Workspace.create"):
+        with mock.patch("argilla_sdk.client._api.Workspace.get") as patch:
+            patch.return_value = mock_return
+            client = rg.Client(api_url="http://test_url", api_key="admin.apikey")
+            workspace = rg.Workspace(name=mock_name, id=mock_uuid)
+            client.workspaces.create(workspace)
+            gotten_workspace = client.get(workspace)
+            patch.assert_called_once_with(workspace.id)
+            assert gotten_workspace == workspace
 
 
 def test_default_client_create_user():
@@ -47,9 +60,7 @@ def test_multiple_clients():
         client = mock_client.return_value
         client.api_url = "http://localhost:6900"
 
-        local_client = rg.Client(
-            api_rul="http://localhost:6900", api_key="admin.apikey"
-        )
+        local_client = rg.Client(api_rul="http://localhost:6900", api_key="admin.apikey")
         assert local_client.api_url == "http://localhost:6900"
 
 
@@ -59,12 +70,8 @@ def test_multiple_clients_create_workspace():
         workspace = mock.Mock()
         client.workspace.create.return_value = workspace
 
-        local_client = rg.Client(
-            api_rul="http://localhost:6900", api_key="admin.apikey"
-        )
-        production_client = rg.Client(
-            api_rul="http://argilla.production.net", api_key="admin.apikey"
-        )
+        local_client = rg.Client(api_rul="http://localhost:6900", api_key="admin.apikey")
+        production_client = rg.Client(api_rul="http://argilla.production.net", api_key="admin.apikey")
         local_client.workspace.create(workspace)
         production_client.workspace.create(workspace)
         assert local_client.workspace.get(name="MyWorkspace") == workspace
@@ -76,21 +83,12 @@ def test_multiple_clients_create_user():
         user = mock.Mock()
         client.user.create.return_value = user
 
-        local_client = rg.Client(
-            api_rul="http://localhost:6900", api_key="admin.apikey"
-        )
-        production_client = rg.Client(
-            api_rul="http://argilla.production.net", api_key="admin.apikey"
-        )
+        local_client = rg.Client(api_rul="http://localhost:6900", api_key="admin.apikey")
+        production_client = rg.Client(api_rul="http://argilla.production.net", api_key="admin.apikey")
         client.user.create(user)
-        user_from_local = client.user.get(
-            name="John Doe", workspace=local_client.workspace.name
-        )
+        user_from_local = client.user.get(name="John Doe", workspace=local_client.workspace.name)
         production_client.user.create(user_from_local)
         assert (
-            production_client.user.get(
-                name="John Doe", workspace=production_client.workspace.name
-            )
-            == user_from_local
+            production_client.user.get(name="John Doe", workspace=production_client.workspace.name) == user_from_local
         )
         assert production_client.user.get(name="John Doe") == user_from_local

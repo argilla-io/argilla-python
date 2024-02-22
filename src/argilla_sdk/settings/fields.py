cefbba3fd09f4063baf8e38058d2004e2b0abcbb
Ben Burtenshaw <ben@argilla.io>
2024-02-22 14:52:26 +0100

    feat: add required and title to text field

--- src/argilla_sdk/settings/fields.py
+++ src/argilla_sdk/settings/fields.py
@@ -1,14 +1,20 @@
+from typing import Optional
+
+
 class FieldBase:
     pass
 
 
 class TextField(FieldBase):
-    def __init__(self, name: str, use_markdown: bool = False):
+    def __init__(self, name: str, use_markdown: bool = False, title: Optional[str] = None, required: bool = True):
         self.name = name
         self.use_markdown = use_markdown
+        self.title = title or name
+        self.required = required
 
     def serialize(self):
         return {
             "name": self.name,
             "use_markdown": self.use_markdown,
+            "required": self.required,
         }

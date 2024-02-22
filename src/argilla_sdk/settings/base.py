7d5d35bccf1703b057908ea6c8c1b444eb164a51
Ben Burtenshaw <ben@argilla.io>
2024-02-22 16:04:32 +0100

    feat: refactor setting to use dataclass

--- src/argilla_sdk/settings/base.py
+++ src/argilla_sdk/settings/base.py
@@ -1,21 +1,17 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
-from argilla_sdk.settings.fields import FieldBase
-from argilla_sdk.settings.questions import QuestionBase
+from attr import dataclass
+
+from argilla_sdk.settings.fields import TextField
+from argilla_sdk.settings.questions import LabelQuestion, MultiLabelQuestion, RankingQuestion, TextQuestion
 
 
+@dataclass
 class Settings:
-    def __init__(
-        self,
-        fields: Optional[List[FieldBase]] = None,
-        questions: Optional[List[QuestionBase]] = None,
-        guidelines: Optional[str] = None,
-        allow_extra_metadata: bool = False,
-    ):
-        self.__guidelines = self.__process_guidelines(guidelines)
-        self.__fields = self.__process_fields(fields) or []
-        self.__questions = self.__process_questions(questions) or []
-        self.__allow_extra_metadata = allow_extra_metadata
+    _fields: Optional[List[TextField]] = []
+    _questions: Optional[List[Union[LabelQuestion, MultiLabelQuestion, RankingQuestion, TextQuestion]]] = []
+    _guidelines: Optional[str] = None
+    _allow_extra_metadata: bool = False
 
     ############################
     # Property methods         #
@@ -24,38 +20,40 @@ class Settings:
     @property
     def guidelines(
         self,
-    ) -> None:
-        return self.__guidelines
+    ) -> Union[str, None]:
+        return self._guidelines
 
     @guidelines.setter
     def guidelines(self, guidelines: str) -> None:
-        self.__guidelines = self.__process_guidelines(guidelines)
+        self._guidelines = self.__process_guidelines(guidelines=guidelines)
 
     @property
     def fields(
         self,
-    ) -> None:
-        return self.__fields
+    ) -> List[TextField]:
+        return self._fields
 
     @fields.setter
-    def fields(self, fields: List[FieldBase]) -> None:
-        self.__fields = self.__process_fields(fields)
+    def fields(self, fields: List[TextField]) -> None:
+        self._fields = self.__process_fields(fields=fields)
 
     @property
     def questions(
         self,
-    ) -> None:
-        return self.__questions
+    ) -> List[Union[LabelQuestion, MultiLabelQuestion, RankingQuestion, TextQuestion]]:
+        return self._questions
 
     @questions.setter
-    def questions(self, questions: List[FieldBase]) -> None:
-        self.__questions = self.__process_questions(questions)
+    def questions(
+        self, questions: List[Union[LabelQuestion, MultiLabelQuestion, RankingQuestion, TextQuestion]]
+    ) -> None:
+        self._questions = self.__process_questions(questions=questions)
 
     @property
     def allow_extra_metadata(
         self,
     ) -> bool:
-        return self.__allow_extra_metadata
+        return self._allow_extra_metadata
 
     ############################
     # Utility Methods          #
@@ -72,7 +70,7 @@ class Settings:
 
     def serialize(self):
         return {
-            "guidelines": self.__guidelines,
-            "fields": [field.serialize() for field in self.__fields],
-            "questions": [question.serialize() for question in self.__questions],
+            "guidelines": self._guidelines,
+            "fields": [field.serialize() for field in self._fields],
+            "questions": [question.serialize() for question in self._questions],
         }

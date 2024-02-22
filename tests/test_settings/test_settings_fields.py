f509b5d8103ec9ac48f4e1727453b1e77bc6501a
Ben Burtenshaw <ben@argilla.io>
2024-02-22 21:07:04 +0100

    test: remove redundant serialisation tests

--- tests/test_settings/test_settings_fields.py
+++ tests/test_settings/test_settings_fields.py
@@ -30,12 +30,3 @@ class TestTextField:
         assert text_field.use_markdown == mock_use_markdown
         assert text_field.title == mock_name
         assert text_field.required == mock_required
-
-    def test_serialize_text_field(self):
-        mock_name = "prompt"
-        mock_use_markdown = True
-        text_field = rg.TextField(name=mock_name, use_markdown=mock_use_markdown)
-        serialized = text_field.serialize()
-        assert serialized["name"] == mock_name
-        assert serialized["use_markdown"] == mock_use_markdown
-        assert serialized["required"] == True

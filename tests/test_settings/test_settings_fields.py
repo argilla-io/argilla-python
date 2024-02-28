085e4c09029acde29ad2ccedfb4ef1d5461fa1fd
Ben Burtenshaw <ben@argilla.io>
2024-02-28 10:46:30 +0100

    test: add tests for names and titles in textfields

--- tests/test_settings/test_settings_fields.py
+++ tests/test_settings/test_settings_fields.py
@@ -1,4 +1,7 @@
+import pytest
 import argilla_sdk as rg
+
+from argilla_sdk.settings.exceptions import InvalidFieldException
 
 
 class TestTextField:
@@ -30,3 +33,43 @@ class TestTextField:
         assert text_field.use_markdown == mock_use_markdown
         assert text_field.title == mock_name
         assert text_field.required == mock_required
+
+    @pytest.mark.parametrize(
+        "name, expected",
+        [
+            ("prompt", "prompt"),
+            ("Prompt", "prompt"),
+            ("Prompt Name", "prompt_name"),
+            ("Prompt Name 2", "prompt_name_2"),
+            ("Prompt Name 2", "prompt_name_2"),
+        ],
+    )
+    def test_name_validator(self, name, expected, mocker):
+        mock_use_markdown = True
+        text_field = rg.TextField(name=name, use_markdown=mock_use_markdown)
+        assert text_field.name == expected
+
+    @pytest.mark.parametrize(
+        "title, name, expected",
+        [
+            (None, "prompt", "prompt"),
+            ("Prompt", "prompt", "Prompt"),
+            ("Prompt", "prompt", "Prompt"),
+        ],
+    )
+    def test_title_validator(self, title, name, expected, mocker):
+        mock_use_markdown = True
+        text_field = rg.TextField(name=name, use_markdown=mock_use_markdown, title=title)
+        assert text_field.title == expected
+
+    @pytest.mark.parametrize(
+        "name",
+        [
+            (""),
+            (" "),
+        ],
+    )
+    def test_name_validator_exception(self, name, mocker):
+        with pytest.raises(InvalidFieldException):
+            mock_use_markdown = True
+            rg.TextField(name=name, use_markdown=mock_use_markdown)

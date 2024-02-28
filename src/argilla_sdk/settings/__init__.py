3e88fd237694329dc3a53ceae8f7f6d1c894ee64
Ben Burtenshaw <ben@argilla.io>
2024-02-28 09:24:02 +0100

    fix: use complete imports in settings init

--- src/argilla_sdk/settings/__init__.py
+++ src/argilla_sdk/settings/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .base import *  # noqa
-from .fields import *  # noqa
-from .questions import *  # noqa
-from .guidelines import *  # noqa
+from argilla_sdk.settings.base import *  # noqa
+from argilla_sdk.settings.fields import *  # noqa
+from argilla_sdk.settings.questions import *  # noqa
+from argilla_sdk.settings.guidelines import *  # noqa

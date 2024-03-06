55062f7bf9a2fed96216ff1b88c9cb32fe486b5e
Ben Burtenshaw <ben@argilla.io>
2024-03-06 15:00:52 +0100

    chore: format main imports

--- src/argilla_sdk/__init__.py
+++ src/argilla_sdk/__init__.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from argilla_sdk.datasets._resource import *  # noqa
-from argilla_sdk.datasets._record import *  # noqa
+from argilla_sdk.client import *  # noqa
+from argilla_sdk.datasets import *  # noqa
 from argilla_sdk.workspaces._resource import *  # noqa
 from argilla_sdk.users._resource import *  # noqa
-from argilla_sdk.client import *  # noqa
 from argilla_sdk.settings import *  # noqa
+from argilla_sdk.suggestion import *  # noqa
+from argilla_sdk.response import *  # noqa

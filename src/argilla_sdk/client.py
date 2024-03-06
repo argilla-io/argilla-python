34219f95a189bf26608d70052f45395299e6150e
Ben Burtenshaw <ben@argilla.io>
2024-03-06 20:29:51 +0100

    enhancement: implement list properties in Argilla client

--- src/argilla_sdk/client.py
+++ src/argilla_sdk/client.py
@@ -11,17 +11,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import TYPE_CHECKING
+
+from typing import TYPE_CHECKING, List
 
 from argilla_sdk import _api
+import argilla_sdk as rg
 
 if TYPE_CHECKING:
-    from argilla_sdk._models import UserModel
+    from argilla_sdk import Workspace
+    from argilla_sdk import Dataset
+    from argilla_sdk import User
 
 __all__ = ["Argilla"]
 
 
 class Argilla(_api.APIClient):
     @property
-    def default_user(self) -> "UserModel":
-        return self._users.get_me()
+    def me(self) -> "UserModel":
+        return rg.User(api=self._users, client=self, model=self._users.get_me())
+
+    @property
+    def workspaces(self) -> List["Workspace"]:
+        return [rg.Workspace(client=self, _model=model) for model in self._workspaces.list()]
+
+    @property
+    def datasets(self) -> List["Dataset"]:
+        return [rg.Dataset(client=self, model=model) for model in self._datasets.list()]
+
+    @property
+    def users(self) -> List["User"]:
+        return [rg.User(client=self, model=model) for model in self._users.list()]

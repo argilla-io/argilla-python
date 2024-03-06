2ec5adda76359cd2631cd121e44838742d9cb1a7
Ben Burtenshaw <ben@argilla.io>
2024-03-06 14:56:41 +0100

    enhancement: expand pydantic models based on API

--- src/argilla_sdk/_models/_resource.py
+++ src/argilla_sdk/_models/_resource.py
@@ -13,9 +13,12 @@ class ResourceModel(BaseModel):
     updated_at: Optional[datetime] = None
 
     @field_serializer("inserted_at", "updated_at", when_used="unless-none")
-    def serialize_datetime(self, value: datetime) -> str:
+    def serialize_datetime(value: datetime) -> str:
         return value.isoformat()
 
     @field_serializer("id", when_used="unless-none")
     def serialize_id(self, value: UUID) -> str:
         return str(value)
+
+    def __hash__(self) -> int:
+        return hash(self.model_dump_json())

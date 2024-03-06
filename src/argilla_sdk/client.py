f69e23a7a59851d6108a4da290335cdeb4d44bc5
Ben Burtenshaw <ben@argilla.io>
2024-03-06 14:59:35 +0100

    feat: migrate CRUD methods out of Argilla client and into Resource base

--- src/argilla_sdk/client.py
+++ src/argilla_sdk/client.py
@@ -11,12 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import TYPE_CHECKING, List, Union, Type
+from typing import TYPE_CHECKING
 
 from argilla_sdk import _api
-from argilla_sdk import Dataset
-from argilla_sdk import User
-from argilla_sdk import Workspace
 
 if TYPE_CHECKING:
     from argilla_sdk._models import UserModel
@@ -28,71 +25,3 @@ class Argilla(_api.APIClient):
     @property
     def default_user(self) -> "UserModel":
         return self._users.get_me()
-
-    def create(self, resource: Union[Workspace, User, Dataset]) -> Union[Workspace, User, Dataset]:
-        """Create a new resource in the API. For example, a new workspace, user, or dataset.
-        Args:
-            resource: Union[Workspace, User, Dataset] - The resource to create
-        Returns:
-            Union[Workspace, User, Dataset] - The created resource
-        """
-        resource_api = self._which_resource_api(resource)
-        response_model = resource_api.create(resource._model)  # type: ignore
-        resource = resource._sync(api=self, model=response_model)
-        return resource
-
-    def get(self, resource: Union[Workspace, User, Dataset]) -> Union[Workspace, User, Dataset]:
-        """Get an resource from the API. For example, a workspace, user, or dataset.
-        Args:
-            resource: Union[Workspace, User, Dataset] - The resource to get
-        Returns:
-            Union[Workspace, User, Dataset] - The requested resource
-        """
-        resource_api = self._which_resource_api(resource)
-        response_model = resource_api.get(resource.id)
-        resource = resource._sync(api=self, model=response_model)
-        return resource
-
-    def list(self, resource_type: Type[Union[Workspace, User, Dataset]]) -> List[Union[Workspace, User, Dataset]]:
-        """List entities from the API. For example, workspaces, users, or datasets.
-        Args:
-            resource: Union[Workspace, User, Dataset] - The resource to list
-        Returns:
-            Union[Workspace, User, Dataset] - The requested entities
-        """
-        resource_api = self._which_resource_api(resource_type)
-        response_models = resource_api.list()
-        return response_models
-
-    def update(self, resource: Union[Workspace, User, Dataset]) -> Union[Workspace, User, Dataset, None]:
-        """Update an resource in the API. For example, a workspace, user, or dataset.
-        Args:
-            resource: Union[Workspace, User, Dataset] - The resource to update
-        Returns:
-            Union[Workspace, User, Dataset] - The updated resource
-        """
-        resource_api = self._which_resource_api(resource)
-        response_model = resource_api.update(resource._model)  # type: ignore
-        resource = resource._sync(api=self, model=response_model)
-        return resource
-
-    def delete(self, resource: Union[Workspace, User, Dataset]) -> None:
-        """Delete an resource from the API. For example, a workspace, user, or dataset.
-        Args:
-            resource: Union[Workspace, User, Dataset] - The resource to delete
-        """
-        resource_api = self._which_resource_api(resource)
-        resource_api.delete(resource.id)
-
-    def _which_resource_api(
-        self, resource: Union[Workspace, User, Dataset]
-    ) -> Union[Union[_api.WorkspacesAPI, _api.UsersAPI, _api.DatasetsAPI], Type[Union[Workspace, User, Dataset]]]:
-        """Determine which resource to use based on the resource type."""
-        if isinstance(resource, Workspace) or resource == Workspace:
-            return self._workspaces
-        elif isinstance(resource, User) or resource == User:
-            return self._users
-        elif isinstance(resource, Dataset) or resource == Dataset:
-            return self._datasets
-        else:
-            raise ValueError("Invalid resource type: must be a Workspace, User, or Dataset.")

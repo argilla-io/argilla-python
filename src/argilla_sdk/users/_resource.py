3e1afcae8940f908ae6c4cef9f6da0d6e23fe465
Ben Burtenshaw <ben@argilla.io>
2024-03-06 21:02:57 +0100

    feat: refactor user init for resource level crud methods

--- src/argilla_sdk/users/_resource.py
+++ src/argilla_sdk/users/_resource.py
@@ -11,12 +11,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from typing import Optional
+
+from argilla_sdk import Argilla
 from argilla_sdk._resource import Resource
-from argilla_sdk._models import UserModel
+from argilla_sdk._models import UserModel, Role
 
 __all__ = ["User"]
 
 
 class User(Resource):
-    def __init__(self, **kwargs) -> None:
-        self._model = UserModel(**kwargs)
+    """Class for interacting with Argilla users"""
+
+    def __init__(
+        self,
+        username: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
+        role: Optional[str] = None,
+        password: Optional[str] = None,
+        client: Optional["Argilla"] = Argilla(),
+        id: Optional[str] = None,
+        _model: Optional[UserModel] = None,
+    ) -> None:
+        """Initializes a User object with a client and a username
+        Args:
+            client (Argilla): The client used to interact with Argilla
+            username (str): The username of the user
+            _model (UserModel): The internal Pydantic model of the user from/to the server
+        Returns:
+            User: The initialized user object
+        """
+        super().__init__(
+            client=client,
+            api=client._users,
+        )
+        if _model is None:
+            if username is None:
+                raise ValueError("A username is required for a user")
+            _model = UserModel(
+                username=username,
+                first_name=first_name or username,
+                last_name=last_name or username,
+                role=role or Role.annotator,
+                password=password,
+                id=id,
+            )
+            self.log(f"Initialized user with username {username}")
+        self._sync(model=_model)

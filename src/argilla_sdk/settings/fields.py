01c287378cbb3aadd380f8e4bc4a0a78ca15ae0c
Ben Burtenshaw <ben@argilla.io>
2024-03-11 19:39:28 +0100

    feat: implement a FieldBase model to setup description and share Field attributes

--- src/argilla_sdk/settings/fields.py
+++ src/argilla_sdk/settings/fields.py
@@ -5,17 +5,18 @@ from typing import Optional
 from argilla_sdk._helpers._log import log
 from argilla_sdk.settings.exceptions import InvalidFieldException
 
+
 class FieldSettings(BaseModel):
     type: str
     use_markdown: Optional[bool] = False
 
 
-class TextField(BaseModel):
+class FieldBase(BaseModel):
     name: str
     title: Optional[str] = None
     required: bool = True
     settings: FieldSettings = FieldSettings(type="text")
-    use_markdown: Optional[bool] = None
+    description: Optional[str] = None
 
     @validator("name")
     def __name_lower(cls, name):
@@ -32,3 +33,7 @@ class TextField(BaseModel):
         validated_title = title or values["name"]
         log(f"TextField title is {validated_title}")
         return validated_title
+
+
+class TextField(FieldBase):
+    use_markdown: Optional[bool] = None

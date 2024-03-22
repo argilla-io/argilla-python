6977c921a8ef92db79dc94bff2a24c3d4ccf74c7
Francisco Aranda <francis@argilla.io>
2024-03-22 01:21:52 +0100

    chore: Add TODO and comment reference

--- src/argilla_sdk/_api/_base.py
+++ src/argilla_sdk/_api/_base.py
@@ -28,6 +28,8 @@ __all__ = ["ResourceAPI"]
 T = TypeVar("T")
 
 
+# TODO: Use ABC and align all the abstract method for the different resources APIs
+# See comment https://github.com/argilla-io/argilla-python/pull/33#discussion_r1532079989
 class ResourceAPI(LoggingMixin, Generic[T]):
     """Base class for all API resources that contains common methods."""
 

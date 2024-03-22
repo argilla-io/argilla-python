c8b20acd1b0a297d133129d98bec06e1219ef80f
Francisco Aranda <francis@argilla.io>
2024-03-22 11:01:22 +0100

    add tests

---
+++ tests/integration/test_list_records.py
@@ -0,0 +1,52 @@
+import random
+from string import ascii_lowercase
+
+import pytest
+
+from argilla_sdk import Argilla, Dataset, Settings, TextField, TextQuestion, Workspace
+
+
+@pytest.fixture
+def client() -> Argilla:
+    return Argilla()
+
+
+@pytest.fixture
+def workspace(client: Argilla) -> Workspace:
+    workspace = client.workspaces("test-workspace")
+    if not workspace.exists():
+        workspace.create()
+    yield workspace
+
+    workspace.delete()
+
+
+@pytest.fixture
+def dataset(client: Argilla, workspace: Workspace) -> Dataset:
+
+    name = "".join(random.choices(ascii_lowercase, k=16))
+    settings = Settings(
+        fields=[TextField(name="text")],
+        questions=[TextQuestion(name="comment", use_markdown=False)],
+    )
+    dataset = Dataset(
+        name=name,
+        workspace_id=workspace.id,
+        settings=settings,
+        client=client,
+    )
+    dataset.publish()
+    yield dataset
+    dataset.delete()
+
+
+def test_list_records_with_start_offset(client: Argilla, dataset: Dataset):
+    dataset.records.add(
+        [
+            {"text": "The record text field", "external_id": 1},
+            {"text": "The record text field", "external_id": 2},
+        ]
+    )
+
+    records = list(dataset.records(start_offset=1))
+    assert len(records) == 1

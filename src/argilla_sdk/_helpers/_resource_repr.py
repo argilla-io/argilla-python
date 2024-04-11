de79b2d4fa01b9a51913780eb054f32da1592690
Ben Burtenshaw <ben@argilla.io>
2024-04-11 07:59:44 +0200

    fix: simplify html_repr

--- src/argilla_sdk/_helpers/_resource_repr.py
+++ src/argilla_sdk/_helpers/_resource_repr.py
@@ -14,7 +14,7 @@ RESOURCE_REPR_CONFIG = {
         "table_name": "Workspaces",
         "len_column": "datasets",
     },
-    "User": {"columns": ["username", "id", "role", "updated_at"], "table_name": "Users", "len_column": None},
+    "User": {"columns": ["username", "id", "role", "updated_at"], "table_name": "Users"},
 }
 
 

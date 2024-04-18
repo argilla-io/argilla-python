9b3a2e4346f0a927c52b0b4fed1d15bc7f03e562
Ben Burtenshaw <ben@argilla.io>
2024-04-18 13:54:01 +0200

    feat: add vectors to models

--- src/argilla_sdk/_models/__init__.py
+++ src/argilla_sdk/_models/__init__.py
@@ -19,7 +19,7 @@ from argilla_sdk._models._dataset import DatasetModel
 from argilla_sdk._models._record import RecordModel
 from argilla_sdk._models._suggestion import SuggestionModel
 from argilla_sdk._models._response import ResponseModel, ResponseStatus
-from argilla_sdk._models._settings._fields import TextFieldModel, FieldSettings, FieldBaseModel
+from argilla_sdk._models._settings._fields import TextFieldModel, FieldSettings, FieldBaseModel, VectorFieldModel
 from argilla_sdk._models._settings._questions import (
     QuestionBaseModel,
     LabelQuestionModel,
@@ -32,3 +32,4 @@ from argilla_sdk._models._settings._questions import (
     RatingQuestionModel,
 )
 from argilla_sdk._models._settings._guidelines import Guidelines
+from argilla_sdk._models._vector import VectorModel

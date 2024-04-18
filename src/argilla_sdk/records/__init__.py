8abacbb7f0f4ad953671d9307144b3e4d3d77bca
Francisco Aranda <francis@argilla.io>
2024-04-18 13:38:51 +0200

    chore: Update imports

--- src/argilla_sdk/records/__init__.py
+++ src/argilla_sdk/records/__init__.py
@@ -1,4 +1,4 @@
 from argilla_sdk.records._resource import Record
-from argilla_sdk.records._dataset_records import DatasetRecords
+from argilla_sdk.records._dataset_records import DatasetRecords, Query
 
-__all__ = ["Record", "DatasetRecords"]
+__all__ = ["Record", "DatasetRecords", "Query"]

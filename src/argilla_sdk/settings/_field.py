8f00dba31d70a90e8dfaec7f0d58f7dd8de3465a
Ben Burtenshaw <ben@argilla.io>
2024-04-18 13:55:10 +0200

    feat: add vectors to settings

--- src/argilla_sdk/settings/_field.py
+++ src/argilla_sdk/settings/_field.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-from argilla_sdk._models import TextFieldModel, FieldSettings
+from argilla_sdk._models import TextFieldModel, FieldSettings, VectorFieldModel
 from argilla_sdk.settings._common import SettingsPropertyBase
 
-__all__ = ["TextField", "FieldType"]
+__all__ = ["TextField", "FieldType", "VectorField"]
 
 
 class TextField(SettingsPropertyBase):
@@ -29,6 +29,36 @@ class TextField(SettingsPropertyBase):
     @property
     def use_markdown(self) -> Optional[bool]:
         return self._model.settings.use_markdown
+
+
+class VectorField(SettingsPropertyBase):
+    _model: VectorFieldModel
+
+    def __init__(
+        self,
+        name: str,
+        dimensions: int,
+        title: Optional[str] = None,
+    ) -> None:
+        """Vector field for use in Argilla `Dataset` `Settings`"""
+        self._model = VectorFieldModel(
+            name=name,
+            title=title,
+            settings=FieldSettings(type="vector"),
+            dimensions=dimensions,
+        )
+
+    @property
+    def dimensions(self) -> int:
+        return self._model.dimensions
+
+    @property
+    def title(self) -> Optional[str]:
+        return self._model.title
+
+    @property
+    def name(self) -> str:
+        return self._model.name
 
 
 FieldType = TextField

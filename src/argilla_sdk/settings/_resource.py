da467c7fa89e060fc310181b0200e39665a1c1f9
Francisco Aranda <francis@argilla.io>
2024-04-23 17:28:08 +0200

    feat: Implement settings.get method

--- src/argilla_sdk/settings/_resource.py
+++ src/argilla_sdk/settings/_resource.py
@@ -17,8 +17,8 @@ from uuid import UUID
 
 from argilla_sdk._models import TextFieldModel, TextQuestionModel
 from argilla_sdk.client import Argilla
-from argilla_sdk.settings._field import FieldType, VectorField
-from argilla_sdk.settings._question import QuestionType
+from argilla_sdk.settings._field import FieldType, VectorField, TextField, field_from_model
+from argilla_sdk.settings._question import QuestionType, question_from_model
 
 if TYPE_CHECKING:
     from argilla_sdk.datasets import Dataset
@@ -37,7 +37,6 @@ class Settings:
         vectors: Optional[List[VectorField]] = None,
         guidelines: Optional[str] = None,
         allow_extra_metadata: bool = False,
-        client: Argilla = Argilla(),
         _dataset: Optional["Dataset"] = None,
     ) -> None:
         """
@@ -61,7 +60,6 @@ class Settings:
         self.__vectors = vectors
 
         self._dataset = _dataset
-        self.__client = client
 
     #####################
     # Properties        #
@@ -118,8 +116,12 @@ class Settings:
     @dataset.setter
     def dataset(self, dataset: "Dataset"):
         self._dataset = dataset
-        self.__allow_extra_metadata = dataset._model.allow_extra_metadata
-        self.__guidelines = dataset._model.guidelines
+        self._dataset._model.allow_extra_metadata = self.allow_extra_metadata
+        self._dataset._model.guidelines = self.guidelines
+
+    @property
+    def _client(self) -> Argilla:
+        return self._dataset._client
 
     @cached_property
     def schema(self) -> dict:
@@ -144,6 +146,14 @@ class Settings:
     #  Public methods   #
     #####################
 
+    def get(self) -> "Settings":
+
+        self.__fetch_fields()
+        self.__fetch_questions()
+        self.__fetch_vectors()
+
+        return self
+
     def create(self) -> "Settings":
         self.__upsert_fields()
         self.__upsert_questions()
@@ -156,6 +166,24 @@ class Settings:
         if isinstance(property, QuestionType):
             return property
         raise ValueError(f"Question with id {question_id} not found")
+
+    def __fetch_fields(self) -> List[FieldType]:
+        models = self._client.api.fields.list(dataset_id=self._dataset.id)
+        self.__fields = [field_from_model(model) for model in models]
+
+        return self.__fields
+
+    def __fetch_questions(self) -> List[QuestionType]:
+        models = self._client.api.questions.list(dataset_id=self._dataset.id)
+        self.__questions = [question_from_model(model) for model in models]
+
+        return self.__questions
+
+    def __fetch_vectors(self) -> List[VectorField]:
+        models = self._client.api.vectors.list(dataset_id=self._dataset.id)
+        self.__vectors = [field_from_model(model) for model in models]
+
+        return self.__vectors
 
     def __update_dataset_related_attributes(self):
         # This flow may be a bit weird, but it's the only way to update the dataset related attributes
@@ -173,17 +201,17 @@ class Settings:
 
     def __upsert_questions(self) -> None:
         for question in self.__questions:
-            question_model = self.__client.api.questions.create(dataset_id=self._dataset.id, question=question._model)
+            question_model = self._client.api.questions.create(dataset_id=self._dataset.id, question=question._model)
             question._model = question_model
 
     def __upsert_fields(self) -> None:
         for field in self.__fields:
-            field_model = self.__client.api.fields.create(dataset_id=self._dataset.id, field=field._model)
+            field_model = self._client.api.fields.create(dataset_id=self._dataset.id, field=field._model)
             field._model = field_model
 
     def __upsert_vectors(self) -> None:
         for vector in self.__vectors:
-            vector_model = self.__client.api.vectors.create(dataset_id=self._dataset.id, vector=vector._model)
+            vector_model = self._client.api.vectors.create(dataset_id=self._dataset.id, vector=vector._model)
             vector._model = vector_model
 
     def serialize(self):
@@ -194,13 +222,18 @@ class Settings:
             "allow_extra_metadata": self.allow_extra_metadata,
         }
 
-    def _set_dataset(self, dataset: "Dataset"):
-        self._dataset = dataset
-        self.__client = dataset._client
-
     #####################
     #  Utility methods  #
     #####################
+
+    def __eq__(self, other: "Settings") -> bool:
+        return self.serialize() == other.serialize()  # TODO: Create proper __eq__ methods for fields and questions
+
+    def __repr__(self) -> str:
+        return (
+            f"Settings(guidelines={self.guidelines}, allow_extra_metadata={self.allow_extra_metadata}, "
+            f"fields={self.fields}, questions={self.questions}, vectors={self.vectors})"
+        )
 
     def __process_fields(self, fields: List[FieldType]) -> List["TextFieldModel"]:
         # TODO: Implement error handling for invalid fields

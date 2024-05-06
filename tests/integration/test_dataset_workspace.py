9c6eb67919d60cb7b8fd8166cb30de6df90026b5
Ben Burtenshaw <ben@argilla.io>
2024-05-06 14:38:20 +0200

    test: update integration tests

--- tests/integration/test_dataset_workspace.py
+++ tests/integration/test_dataset_workspace.py
@@ -3,34 +3,13 @@ import random
 import pytest
 
 import argilla_sdk as rg
+from argilla_sdk._exceptions import NotFoundError
 
 
 @pytest.fixture
 def client() -> rg.Argilla:
-    client = rg.Argilla(api_url="http://localhost:6900", api_key="owner.apikey")
+    client = rg.Argilla(api_url="http://localhost:6900", api_key="argilla.apikey")
     return client
-
-
-def test_dataset_with_workspace_id(client: rg.Argilla):
-    workspace_id = client.workspaces[0].id
-    dataset = rg.Dataset(
-        name=f"test_{random.randint(0, 1000)}",
-        settings=rg.Settings(
-            fields=[
-                rg.TextField(name="text"),
-            ],
-            questions=[
-                rg.TextQuestion(name="response"),
-            ],
-        ),
-        workspace_id=workspace_id,
-        client=client,
-    )
-    dataset.publish()
-    assert dataset.id is not None
-    assert dataset.exists()
-    assert dataset.is_published
-    assert dataset.workspace_id == workspace_id
 
 
 def test_dataset_with_workspace(client: rg.Argilla):
@@ -53,6 +32,45 @@ def test_dataset_with_workspace(client: rg.Argilla):
     assert dataset.exists()
     assert dataset.is_published
     assert dataset.workspace_id == ws.id
+
+
+def test_dataset_with_workspace_name(client: rg.Argilla):
+    ws = client.workspaces[0]
+    dataset = rg.Dataset(
+        name=f"test_{random.randint(0, 1000)}",
+        settings=rg.Settings(
+            fields=[
+                rg.TextField(name="text"),
+            ],
+            questions=[
+                rg.TextQuestion(name="response"),
+            ],
+        ),
+        workspace=ws.name,
+        client=client,
+    )
+    dataset.publish()
+    assert dataset.id is not None
+    assert dataset.exists()
+    assert dataset.is_published
+    assert dataset.workspace_id == ws.id
+
+
+def test_dataset_with_incorrect_workspace_name(client: rg.Argilla):
+    with pytest.raises(expected_exception=NotFoundError):
+        rg.Dataset(
+            name=f"test_{random.randint(0, 1000)}",
+            settings=rg.Settings(
+                fields=[
+                    rg.TextField(name="text"),
+                ],
+                questions=[
+                    rg.TextQuestion(name="response"),
+                ],
+            ),
+            workspace=f"non_existing_workspace_{random.randint(0, 1000)}",
+            client=client,
+        )
 
 
 def test_dataset_with_default_workspace(client: rg.Argilla):

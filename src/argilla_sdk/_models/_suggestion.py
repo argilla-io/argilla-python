0520516b8a382bdc7e86624c20babf24e4c56b04
Francisco Aranda <francis@argilla.io>
2024-05-07 16:34:57 +0200

    feat: Support a list of values for score
    Also the init method for Suggestion resource is aligned to the Response init method
    where question_name is the first argument.

--- src/argilla_sdk/_models/_suggestion.py
+++ src/argilla_sdk/_models/_suggestion.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Literal
+from typing import Any, Optional, Literal, Union, List
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, field_serializer
@@ -11,15 +11,15 @@ class SuggestionModel(BaseModel):
 
     question_name: Optional[str] = None
     type: Optional[Literal["model", "human"]] = None
-    score: Optional[float] = None
+    score: Union[float, List[float], None] = None
     agent: Optional[str] = None
     id: Optional[UUID] = uuid4()
     question_id: Optional[UUID] = None
 
     @field_serializer("id", when_used="unless-none")
-    def serialize_id(value: UUID) -> str:
+    def serialize_id(self, value: UUID) -> str:
         return str(value)
 
     @field_serializer("question_id", when_used="unless-none")
-    def serialize_question_id(value: UUID) -> str:
+    def serialize_question_id(self, value: UUID) -> str:
         return str(value)

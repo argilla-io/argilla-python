290337112050edd05f0c87c29eed2abe5b58adfb
Ben Burtenshaw <ben@argilla.io>
2024-05-08 11:01:30 +0200

    test: expand tests to include missing fields and questions

--- tests/integration/test_empty_settings.py
+++ tests/integration/test_empty_settings.py
@@ -3,7 +3,7 @@ from string import ascii_lowercase
 
 import pytest
 
-from argilla_sdk import Argilla, Dataset, Settings, Workspace
+from argilla_sdk import Argilla, Dataset, Settings, Workspace, TextQuestion, TextField
 from argilla_sdk._exceptions import SettingsError
 
 @pytest.fixture
@@ -27,6 +27,40 @@ def workspace(client: Argilla) -> Workspace:
 def test_dataset_empty_settings(client: Argilla, workspace: Workspace) -> Dataset:
     name = "".join(random.choices(ascii_lowercase, k=16))
     settings = Settings()
+    dataset = Dataset(
+        name=name,
+        workspace=workspace.name,
+        settings=settings,
+        client=client,
+    )
+    with pytest.raises(expected_exception=SettingsError):
+        dataset.publish()
+
+
+def test_dataset_no_fields(client: Argilla, workspace: Workspace) -> Dataset:
+    name = "".join(random.choices(ascii_lowercase, k=16))
+    settings = Settings(
+        questions=[
+            TextQuestion(name="text_question"),
+        ],
+    )
+    dataset = Dataset(
+        name=name,
+        workspace=workspace.name,
+        settings=settings,
+        client=client,
+    )
+    with pytest.raises(expected_exception=SettingsError):
+        dataset.publish()
+    
+
+def test_dataset_no_questions(client: Argilla, workspace: Workspace) -> Dataset:
+    name = "".join(random.choices(ascii_lowercase, k=16))
+    settings = Settings(
+        fields=[
+            TextField(name="text_field"),
+        ],
+    )
     dataset = Dataset(
         name=name,
         workspace=workspace.name,

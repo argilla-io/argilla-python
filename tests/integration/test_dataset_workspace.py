395cf84f08358fe13a1bd0e10c3e683a06995145
burtenshaw <ben@argilla.io>
2024-05-08 12:16:07 +0200

    [FIX] deal-with-workspace-in-dataset-sequence (#138)
    * test: add tests for different workspace case in sequences
    
    * fix: unify datasets workspace with Dataset resource on name etc
    
    * Update tests/integration/test_dataset_workspace.py
    
    Co-authored-by: Francisco Aranda <francis@argilla.io>
    
    * Update tests/integration/test_dataset_workspace.py
    
    Co-authored-by: Francisco Aranda <francis@argilla.io>
    
    * Update tests/integration/test_dataset_workspace.py
    
    Co-authored-by: Francisco Aranda <francis@argilla.io>
    
    ---------
    
    Co-authored-by: Francisco Aranda <francis@argilla.io>

--- tests/integration/test_dataset_workspace.py
+++ tests/integration/test_dataset_workspace.py
@@ -12,6 +12,27 @@ def client() -> rg.Argilla:
     return client
 
 
+@pytest.fixture
+def dataset(client: rg.Argilla):
+    ws = client.workspaces[0]
+    dataset = rg.Dataset(
+        name=f"test_{random.randint(0, 1000)}",
+        settings=rg.Settings(
+            fields=[
+                rg.TextField(name="text"),
+            ],
+            questions=[
+                rg.TextQuestion(name="response"),
+            ],
+        ),
+        workspace=ws,
+        client=client,
+    )
+    dataset.publish()
+    yield dataset
+    dataset.delete()
+
+
 def test_dataset_with_workspace(client: rg.Argilla):
     ws = client.workspaces[0]
     dataset = rg.Dataset(
@@ -91,3 +112,21 @@ def test_dataset_with_default_workspace(client: rg.Argilla):
     assert dataset.exists()
     assert dataset.is_published
     assert dataset.workspace_id == client.workspaces[0].id
+
+
+def test_retrieving_dataset(client: rg.Argilla, dataset: rg.Dataset):
+    ws = client.workspaces[0]
+    dataset = client.datasets(dataset.name, workspace=ws)
+    assert dataset.exists()
+
+
+def test_retrieving_dataset_on_name(client: rg.Argilla, dataset: rg.Dataset):
+    ws = client.workspaces[0]
+    dataset = client.datasets(dataset.name, workspace=ws.name)
+    assert dataset.exists()
+
+
+def test_retrieving_dataset_on_default(client: rg.Argilla, dataset: rg.Dataset):
+    ws = client.workspaces[0]
+    dataset = client.datasets(dataset.name)
+    assert dataset.exists()    

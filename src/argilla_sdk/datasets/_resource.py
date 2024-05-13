205c5c9883835855f7ba8fd0c440dabb1df84edf
Francisco Aranda <francis@argilla.io>
2024-05-13 15:45:23 +0200

    refactor: Use the default argilla client instead of creating a new one

--- src/argilla_sdk/datasets/_resource.py
+++ src/argilla_sdk/datasets/_resource.py
@@ -43,7 +43,7 @@ class Dataset(Resource):
         name: Optional[str] = None,
         workspace: Optional[Union["Workspace", str]] = None,
         settings: Optional[Settings] = None,
-        client: Optional["Argilla"] = Argilla(),
+        client: Optional["Argilla"] = None,
         _model: Optional[DatasetModel] = None,
     ) -> None:
         """Initalizes a Dataset with a client and model
@@ -53,6 +53,7 @@ class Dataset(Resource):
             settings (Settings): Settings class to be used to configure the dataset.
             client (Argilla): Instance of Argilla to connect with the server.
         """
+        client = client or Argilla._get_default()
         super().__init__(client=client, api=client.api.datasets)
         if name is None:
             name = f"dataset_{uuid4()}"

2df4b691c22d95a861c4981577c098f355f32fcd
Ben Burtenshaw <ben@argilla.io>
2024-05-22 12:39:21 +0200

    fix: stop flake8 on settings questions imports in init

--- src/argilla_sdk/_models/_settings/_questions/__init__.py
+++ src/argilla_sdk/_models/_settings/_questions/__init__.py
@@ -11,17 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# flake8: noqa
 from typing import Union
 
-from argilla_sdk._models._settings._questions._label_selection import LabelQuestionModel
+from argilla_sdk._models._settings._questions._label_selection import LabelQuestionModel, LabelQuestionSettings
 from argilla_sdk._models._settings._questions._multi_label_selection import (
     MultiLabelQuestionModel,
+    MultiLabelQuestionSettings,
 )
-from argilla_sdk._models._settings._questions._rating import RatingQuestionModel
-from argilla_sdk._models._settings._questions._ranking import RankingQuestionModel
-from argilla_sdk._models._settings._questions._text import TextQuestionModel
-from argilla_sdk._models._settings._questions._base import QuestionBaseModel
+from argilla_sdk._models._settings._questions._rating import RatingQuestionModel, RatingQuestionSettings
+from argilla_sdk._models._settings._questions._ranking import RankingQuestionModel, RankingQuestionSettings
+from argilla_sdk._models._settings._questions._text import TextQuestionModel, TextQuestionSettings
+from argilla_sdk._models._settings._questions._base import QuestionBaseModel, QuestionSettings
+from argilla_sdk._models._settings._questions._span import SpanQuestionModel, SpanQuestionSettings
 
 QuestionModel = Union[
     LabelQuestionModel,

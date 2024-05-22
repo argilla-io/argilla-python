8b6ca02b51cc89825194ecfd227e9401b2ae426c
Ben Burtenshaw <ben@argilla.io>
2024-05-22 09:01:29 +0200

    chore: document and format export methods

--- src/argilla_sdk/datasets/_export.py
+++ src/argilla_sdk/datasets/_export.py
@@ -41,24 +41,31 @@ class DiskImportExportMixin(ABC):
     _default_dataset_path = "dataset.json"
 
     def to_disk(self, path: str) -> str:
-        """Exports the dataset to disk in the given path.
+        """Exports the dataset to disk in the given path. The dataset is exported as a directory containing the dataset model, settings and records as json files.
 
         Args:
-            path (str): The path to export the dataset to.
+            path (str): The path to export the dataset to. Must be an empty directory.
         """
 
-        dir_path, dataset_path, settings_path, records_path = self._define_child_paths(path=path)
-        os.makedirs(path, exist_ok=True)
+        dataset_path, settings_path, records_path = self._define_child_paths(path=path)
+        
+        # Deal with path creation and use
+        if os.path.exists(path):
+            if not os.path.isdir(path):
+                raise ValueError(f"Path {path} is not a directory")
+            if os.listdir(path):
+                raise ValueError(f"Directory {path} is not empty")
+        else:
+            os.makedirs(name=path, exist_ok=True)
 
+        # Export the dataset model, settings and records
         with open(dataset_path, "w") as f:
             json.dump(self._model.model_dump(), f)
-
         self.settings.to_json(path=settings_path)
-
         if self.exists():
             self.records.to_json(path=records_path)
 
-        return dir_path
+        return path
 
     @classmethod
     def from_disk(
@@ -68,14 +75,30 @@ class DiskImportExportMixin(ABC):
         target_name: Optional[str] = None,
         client: Optional["Argilla"] = None,
     ) -> "Dataset":
+        """ Imports a dataset from disk as a directory containing the dataset model, settings and records.
+            The directory should be defined using the `to_disk` method.
+            
+            Parameters:
+            path (str): The path to the directory containing the dataset model, settings and records.
+            target_workspace (Union[Workspace, str], optional): The workspace to import the dataset to. Defaults to None and default workspace is used.
+            target_name (str, optional): The name to assign to the new dataset. Defaults to None and the dataset's source name is used, unless it already exists, in which case a unique UUID is appended.
+            client (Argilla, optional): The client to use for the import. Defaults to None and the default client is used.
+        """
+        
         client = client or Argilla._get_default()
 
-        path, dataset_path, settings_path, records_path = cls._define_child_paths(path=path)
+        dataset_path, settings_path, records_path = cls._define_child_paths(path=path)
+        
+        logging.info(f"Loading dataset from {dataset_path}")
+        logging.info(f"Loading settings from {settings_path}")
+        logging.info(f"Loading records from {records_path}")
 
-        with open(dataset_path, "r") as f:
-            dataset_model = json.load(f)
-            dataset_model = DatasetModel(**dataset_model)
+        if not os.path.exists(dataset_path):
+            raise FileNotFoundError(f"Dataset model not found at {dataset_path}")
+        
+        dataset_model = cls._load_dataset_model(path=dataset_path)
 
+        # Get the relevant workspace_id of the incoming dataset
         if isinstance(target_workspace, str):
             workspace_id = client.workspaces(target_workspace).id
         elif isinstance(target_workspace, Workspace):
@@ -85,6 +108,7 @@ class DiskImportExportMixin(ABC):
             workspace_id = client.workspaces.default.id
         dataset_model.workspace_id = workspace_id
 
+        # Get a relevant and uniwue name for the incoming dataset.
         if target_name:
             logging.warning(f"Changing dataset name from {dataset_model.name} to {target_name}")
             dataset_model.name = target_name
@@ -92,10 +116,9 @@ class DiskImportExportMixin(ABC):
             logging.warning(f"Loaded dataset name {dataset_model.name} already exists. Changing to unique UUID.")
             dataset_model.name = f"{dataset_model.name}_{uuid4()}"
 
+        # Create the dataset and load the settings and records
         dataset = cls.from_model(model=dataset_model, client=client)
-
         dataset.settings = Settings.from_json(path=settings_path)
-
         if os.path.exists(records_path):
             dataset.records.from_json(path=records_path)
         return dataset
@@ -103,14 +126,25 @@ class DiskImportExportMixin(ABC):
     ############################
     # Utility methods
     ############################
+    
+    def _persist_dataset_model(self, path: Path):
+        with open(path, "w") as f:
+            json.dump(self._model.model_dump(), f)
+    
+    @classmethod
+    def _load_dataset_model(cls, path: Path):
+        with open(path, "r") as f:
+            dataset_model = json.load(f)
+            dataset_model = DatasetModel(**dataset_model)
+        return dataset_model
 
     @classmethod
-    def _define_child_paths(cls, path: Union[Path, str]) -> Tuple[Path, Path, Path, Path]:
+    def _define_child_paths(cls, path: Union[Path, str]) -> Tuple[Path, Path, Path]:
         path = Path(path)
         dataset_path = path / cls._default_dataset_path
         settings_path = path / cls._default_settings_path
         records_path = path / cls._default_records_path
-        return path, dataset_path, settings_path, records_path
+        return dataset_path, settings_path, records_path
 
     ############################
     # Abstracted Dataset Methods
